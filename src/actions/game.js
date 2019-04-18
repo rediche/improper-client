5604c1201021b741c4e156cf652a30ca06527e37
Rediche <weblrmail@gmail.com>
2019-04-18 10:50:19 +0200

    Redux: Update player type

--- src/actions/game.js
+++ src/actions/game.js
@@ -1,10 +1,18 @@
 import { navigate } from './app.js';
 
 export const UPDATE_GAMECODE = 'UPDATE_GAMECODE';
+export const UPDATE_PLAYER_TYPE = 'UPDATE_PLAYER_TYPE';
 
 export const updateGameCode = (gameCode) => {
   return {
     type: UPDATE_GAMECODE,
     gameCode
+  };
+};
+
+const updatePlayerType = (playerType) => {
+  return {
+    type: UPDATE_PLAYER_TYPE,
+    playerType
   };
 };
--- src/reducers/game.js
+++ src/reducers/game.js
@@ -1,9 +1,16 @@
 import {
-  UPDATE_GAMECODE
+  UPDATE_GAMECODE,
+  UPDATE_PLAYER_TYPE
 } from '../actions/game.js';
 
+const PLAYER_TYPES = {
+  HOST: 'HOST',
+  CLIENT: 'CLIENT'
+};
+
 const INITIAL_STATE = {
-  gameCode: ''
+  gameCode: '',
+  playerType: PLAYER_TYPES.CLIENT
 };
 
 const game = (state = INITIAL_STATE, action) => {
@@ -12,6 +19,11 @@ const game = (state = INITIAL_STATE, action) => {
       return {
         ...state,
         gameCode: action.gameCode
+      };
+    case UPDATE_PLAYER_TYPE:
+      return {
+        ...state,
+        playerType: action.playerType
       };
     default:
       return state;
