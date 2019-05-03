a22e822d300fae933f824807a7e10f8af09c0f2d
Rediche <weblrmail@gmail.com>
2019-05-03 10:55:51 +0200

    Change font size on card

--- src/components/game-card.js
+++ src/components/game-card.js
@@ -48,7 +48,7 @@ class GameCard extends LitElement {
           width: 100%;
           height: 100%;
           border-radius: var(--game-card-border-radius);
-          font-size: 32px;
+          font-size: 24px;
           font-weight: 700;
           display: flex;
           flex-direction: column;
