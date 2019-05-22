fe0c42a04e9dde065f6b15a47737110ce6226aeb
Rediche <weblrmail@gmail.com>
2019-05-22 12:31:31 +0200

    Change src SW cache to be dynamic for faster initial load

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -1,12 +1,15 @@
 module.exports = {
   staticFileGlobs: [
     'manifest.json',
-    'node_modules/socket.io-client/dist/socket.io.slim.js',
-    'src/**/*',
+    'node_modules/socket.io-client/dist/socket.io.slim.js'
   ],
   runtimeCaching: [
     {
       urlPattern: /\/@webcomponents\/webcomponentsjs\//,
+      handler: 'fastest'
+    },
+    {
+      urlPattern: /\/src\//,
       handler: 'fastest'
     }
   ]
